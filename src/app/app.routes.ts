d1732aa11ef05d3b4738e5a947026f4c45d2843d
Vladimir Roguljic <vroguljic1@gmail.com>
2023-12-01 08:52:00 +0100

    protect route with auth guard, redirect on keycloak login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AdministrationPanelComponent } from '../administration-panel/administration-panel.component';
+import { AuthGuard } from '../guard/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: AdministrationPanelComponent,
+    canActivate: [AuthGuard],
   },
 ];
